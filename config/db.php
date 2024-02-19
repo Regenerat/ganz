b304f27275109fbc0258b544045ca3e75912cf99
Megira <regenerat.55@mail.ru>
2024-02-19 23:21:38 +0300

    Изменил конфиг БД

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1:1403;dbname=yii',
+    'dsn' => 'mysql:host=127.0.0.1:1403;dbname=yii2',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
