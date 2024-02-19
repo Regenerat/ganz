9eae6bdf330ad9f1d1ffbf178d9f08e4941845bd
Megira <regenerat.55@mail.ru>
2024-02-20 01:20:45 +0300

    добавил авторизацию

--- models/User.php
+++ models/User.php
@@ -14,7 +14,7 @@ use Yii;
  * @property string $login
  * @property string $password
  */
-class User extends \yii\db\ActiveRecord
+class User extends \yii\db\ActiveRecord implements \yii\web\IdentityInterface
 {
     /**
      * {@inheritdoc}
@@ -31,12 +31,12 @@ class User extends \yii\db\ActiveRecord
     {
         return [
             [['fio', 'number', 'email', 'login', 'password'], 'required'],
-            [['fio', 'email', 'login', 'password'], 'string', 'max' => 50],
+            [['fio', 'email', 'login', 'password'], 'string', 'max' => 255],
             [['number'], 'string', 'max' => 11],
         ];
     }
 
-    /**
+    /** 
      * {@inheritdoc}
      */
     public function attributeLabels()
@@ -49,5 +49,88 @@ class User extends \yii\db\ActiveRecord
             'login' => 'Login',
             'password' => 'Password',
         ];
+    }
+
+    /**
+     * Gets query for [[Reports]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    
+    /**
+     * Функция поиска пользователя по логину и паролю
+     * @param string $login Логин пользователя
+     * @param string $password Пароль пользователя
+     * @return User|null Возвращает пользователя или null, если соответствующего пользователя нет
+     */
+    public static function login($login, $password) {
+        // метод find() возвращает Query-объект (объект построения запроса в бд)
+        // метод where([{column} => {value}]) добавляет условие и возвращает Query-объект (объект построения запроса в бд)
+        // метод one() возвращает экземпляр соответствующего класса, либо null, если не найдено ни одной записи
+        // Может быть заменено на метод findOne([{column} => {value}]), который является alias для find()->where([{column} => {value}])->one()
+        // Происходит поиск пользователя по его логину
+        $user = static::find()->where(['login' => $login])->one();
+
+        // Проверка на пользователя и на совпадение его пароля
+        if ($user && $user->validatePassword($password)) {
+            return $user;
+        }
+
+        // Иначе возвращать null
+        return null;
+    }
+
+    /**
+     * Скопировано из User.php.dist
+     * В будущем будет изменено для сравнения пароля по хешу
+     * Validates password
+     *
+     * @param string $password password to validate
+     * @return bool if password provided is valid for current user
+     */
+    public function validatePassword($password)
+    {
+        return $this->password === $password;
+    }
+
+     /**
+     * {@inheritdoc}
+     */
+    public static function findIdentity($id)
+    {
+        // Поиск пользователя по id. Может быть заменено на alias static::findOne(['id' => $id]);
+        return static::find()->where(['id' => $id])->one();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAuthKey()
+    {
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validateAuthKey($authKey)
+    {
+        return null;
     }
 }
